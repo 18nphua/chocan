491947183179de85093c1b0d77859e85ad409720
Jordan Dexter <jordan@ad.innsoft.com>
2024-11-30 21:30:49 -0800

    Database API v1. run 'python.exe ResetAPI.py' to reset API. See notes in 'ResetAPI.py' for more information

--- DatabaseApi.py
+++ DatabaseApi.py
@@ -3,7 +3,6 @@ File: sql_api.py
 Name: Jordan Dexter
 Desc: This file defines the functionality of the mySQL api used by chocan to enable standerized communication
       between providers and managers with the various databases found in Chocan network.
-
 """
 
 import sqlite3
@@ -24,7 +23,6 @@ class db_client():
         return 1
     
     # MEMBER FUNCTIONALITY
-
     def add_member(self,
                    name: str,
                    phone_number : str,
@@ -32,9 +30,6 @@ class db_client():
                    city: str,
                    state : str,
                    zip_code : int):
-        #maybe in the future it will check if there already exists a person with the same
-        #name, phone number, and address and won't let you add the user.(make another 
-        #function called force_add_member to add the same users?)
 
         id_num = 1 # self.gen_id()
         result = self.mem_cur.execute(f"""INSERT INTO members VALUES 
@@ -43,16 +38,11 @@ class db_client():
         return
     
     def edit_member(self, attribute : str, value):
-        #check if the member exists in the database
-
-        # to implement the changing of the data
+        # to implement
         return
     
     def remove_member(self, name):
-        #maybe in the future, it will check if something was removed or not, return true and false
-
-        #delete the member
-        self.mem_cur.execute(f"DELETE FROM members WHERE name = ?", (name,))#later change it to ID, but for now its name
+        # to implement
         return
     
     # PROVIDER FUNCTIONALITY
---
+++ ResetAPI.py
@@ -0,0 +1,45 @@
+import subprocess
+import sqlite3
+from time import sleep
+
+"""
+This file recreates the database files from scratch for use while debugging.
+
+Usage: python.exe ResetAPI.py
+
+Note: This will delete all database files and recreate them.
+"""
+
+databases   = ['members.db', 'providers.db', 'services_provided_log.db']
+schemas     = ['(id int, name varchar, street_address varchar, city varchar, state varchar, zip_code int, balance float, PRIMARY KEY (id))',
+               '(id int, name varchar, street_address varchar, city varchar, state varchar, zip_code int, PRIMARY KEY(id))',
+               '(date_service_provided date, date_service_logged date, provider_id, member_id int, member_name varchar, service_code int, fee float)']
+
+confirm = input("This will delete all data in ALL database files. Are you sure you'd like to continue (Y/n?: ")
+if confirm != 'Y':
+    exit()
+
+for i in range(3):
+    print(f"deleting {databases[i]}..")
+
+    cmd = f"rm {databases[i]} -Force"
+    subprocess.run(['powershell', '-Command', cmd], capture_output=True)
+sleep(1)
+print("done.")
+
+for i in range(3):
+    print(f"creating {databases[i]}...")
+
+    cmd2 = f"New-Item {databases[i]} -ItemType File"
+    subprocess.run(['powershell', '-Command', cmd2], capture_output=True)
+    db = sqlite3.connect(databases[i])
+    cur = db.cursor()
+    result = cur.execute(f"CREATE TABLE {databases[i].strip('.db')} {schemas[i]}")
+    
+
+sleep(1)
+print("done.")
+
+# Keys setup
+
+
