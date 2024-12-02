1b488d94398735c210568da08745d73e3f5b0c65
Filipp <fsuprun@pdx.edu>
2024-12-01 18:19:03 -0800

    member functions in database update
    Added edit_member() and remove_member().
    
    I also made it so that you can't add duplicates of the same member into the database for add_member(). What that means is that you can't add a member with the exact same information to an existing member in the database.

--- DatabaseApi.py
+++ DatabaseApi.py
@@ -20,6 +20,10 @@ class db_client():
         self.spl_cur        = self.spl_db.cursor()
     
     # MEMBER FUNCTIONALITY
+
+    #this function will be used to add new members to the database,
+    #along with stopping from making duplicate members. Returns true if a 
+    #new member was added to the database, false otherwise.
     def add_member(self,
                    name: str,
                    phone_number : int,
@@ -28,23 +32,88 @@ class db_client():
                    state : str,
                    zip_code : int):
 
-        result = self.mem_cur.execute(f"""INSERT INTO members (id, name, phone_number, street_address, city, state, zip_code) VALUES 
+        #check if there exists a member with the same name, phone number, street adress, etc.
+        #to prevent duplications of members in the database.
+        self.mem_cur.execute("""SELECT EXISTS (SELECT 1 FROM members WHERE name = ? 
+                                AND phone_number = ? AND street_address = ? 
+                                AND city = ? AND state = ? AND zip_code = ?)""", 
+                                (name, phone_number, street_address, city, state, zip_code))
+        
+        if self.mem_cur.fetchone()[0] == 0:#if person doesn't exist in the database -> add to database
+            #add the member to the data base
+            result = self.mem_cur.execute(f"""INSERT INTO members (id, name, phone_number, street_address, city, state, zip_code) VALUES 
                 ( NULL, '{name}', {phone_number}, '{street_address}', '{city}', '{state}', {zip_code});
                 """)
-        self.mem_cur.execute("COMMIT")
-        return
+            self.mem_cur.execute("COMMIT")
+
+            return True# a new entry was added
+        
+        #if the person deos already exist in the database then don't add
+        return False #no new entries was added
+    
     
     def print_members(self):
         result = self.mem_cur.execute("SELECT * FROM members")
         print(result.fetchall())
     
-    def edit_member(self, attribute : str, value):
-        # to implement
-        return
+
+    #the function requires three parameters, the target member(using ID), the value that will be
+    #changed(attribute, example is name) and the parameter value is what will be used to replace
+    #the old value. Returns true if a change happended and false if nothing was changed.
+    def edit_member(self, target_id : int, attribute : str, value):
+        # match case
+        match attribute:
+            # change ID
+            case "id_num":
+                self.mem_cur.execute("UPDATE members SET id_num = ? WHERE id_num = ?", (value, target_id))
+
+                # change name
+            case "name":
+                self.mem_cur.execute("UPDATE members SET name = ? WHERE id_num = ?", (value, target_id))
+
+                # change phone number
+            case "phone_number":
+                self.mem_cur.execute("UPDATE members SET phone_number = ? WHERE id_num = ?", (value, target_id))
+
+                # change steet address
+            case "street_address":
+                self.mem_cur.execute("UPDATE members SET street_address = ? WHERE id_num = ?", (value, target_id))
+
+                # change city
+            case "city":
+                self.mem_cur.execute("UPDATE members SET city = ? WHERE id_num = ?", (value, target_id))
+
+                # change state
+            case "state":
+                self.mem_cur.execute("UPDATE members SET state = ? WHERE id_num = ?", (value, target_id))
+
+                # change zip code
+            case "zip_code":
+                self.mem_cur.execute("UPDATE members SET zip_code = ? WHERE id_num = ?", (value, target_id))
+            case _:
+                print("Unkown attribute")
+                return False#nothing was changed
+
+        #this determines wether or not the database actually edited a member or not
+        if self.mem_cur.rowcount == 0:#didn't change
+            return False
+        else:
+            return True#a member was edited
     
-    def remove_member(self, name):
-        # to implement
-        return
+
+    #the function will remove the member that has the same id as member_ID, will return true if removed
+    # or false if nothing was removed. One small problem with this function is that it will remove all
+    #members with the same ID, however this shouldn't be a problem since all members have a unique ID.
+    def remove_member(self, member_ID):
+        #delete the member from the database
+        self.mem_cur.execute("DELETE FROM members WHERE id_num = ?", (member_ID,))
+            
+        #this determines wether or not the database actually removed a member or not
+        if self.mem_cur.rowcount == 0:#didn't change, no members removed
+            return False
+        else:
+            return True#a member was removed
+        
     
     # PROVIDER FUNCTIONALITY
     def add_provider(self,
