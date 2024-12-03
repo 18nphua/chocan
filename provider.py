81cb606addb79bc16e11c40f5c922596fb966113
Huan Nguyen <nhuan915@gmail.com>
2024-12-03 15:41:14 -0800

    Implementing provider functions

--- provider.py
+++ provider.py
@@ -8,28 +8,33 @@ Desc: This file contains the Provider class, which will manage all provider-spec
 import valid as v
 import DatabaseApi as db
 
-PROVIDER_NUM_LENGTH = 9
+PROVIDER_ID_LENGTH = 9
 
 class Provider():
     def __init__(self) -> None:
-        self.__provider_num = 0
-        self.__name = " "
-        self.__street = " "
-        self.__city = " "
-        self.__state = " "
-        self.__zip_code = 0
+        self.provider_id = 0
+        self.provider_name = None
+        self.database = db.db_client()
 
-    def read_provider_num(self, database: db.db_client) -> bool:
-        self.__provider_num = v.read_int("Please enter your provider number: ")
-        identity_verified = False
+    def read_provider_num(self, database: db.db_client) -> bool: 
+        validated = False
+        self.provider_num = v.read_int("Please enter your provider number: ")
 
-        while len(str(self.__provider_num)) != PROVIDER_NUM_LENGTH:
+        while len(str(self.provider_id)) != PROVIDER_ID_LENGTH:
             print(f"Please enter a 9-digit number.\n")
-            self.__provider_num = v.read_int("Please enter your provider number: ")
+            self.provider_id = v.read_int("Please enter your provider number: ")
 
-        #identity_verified = database.retrieve_provider_info(self)        
-        
-        return identity_verified
+        self.provider_name = self.database.prov_get_name_from_id(self.self.provider_id)        
+
+        if self.provider_name is not None:
+            print("Validated.")
+            print(f"Welcome {self.provider_name}")
+            validated = True
+
+        else:
+            print("Invalid provider number entered.")
+
+        return validated
 
     def generate_bill(self) -> None:
         pass
