74df6d5b91790b229228abb5632b7ffbec804a26
brachang <brachang@pdx.edu>
2024-12-03 15:05:59 -0800

    Implementing ChocAn Interface

--- ChocAnInterface.py
+++ ChocAnInterface.py
@@ -5,24 +5,47 @@ Desc: This is the Interface Class which will work as an interface between
       the user and the manager/provider classes.
 
 """
+import valid as v
+import provider as prov
+
 from ChocAnServiceCoordinator import *
 
 class chocan_interface():
     def __init__(self):
-        user_cord = chocan_service_cord()
+        #user_cord = chocan_service_cord()
         self.user_input = 0
+        provider = prov.Provider()
 
     #Menu that shows up on startup
     def start_menu(self):
-        pass
+        term_choice = 1
+
+        while(term_choice != 0):
+            print("Welcome to the ChocAn Terminal!\n")
+            #print("Please choose which terminal to use\n")
+            print("1. Provider Terminal")
+            print("2. Manager Terminal")
+            print("0. Quit\n")
+            term_choice = v.read_int("Please choose which terminal to use: ")
+            
+            if (term_choice == 1):
+                self.provider_menu()
+            elif (term_choice == 2):
+                self.manager_menu()
+            elif (term_choice < 0 or term_choice > 3):
+                print("\nInvalid choice selected.")
 
     #Provider menu this will work with the provider class
     def provider_menu(self):
-        pass
+        prov_choice = 1
+        while (prov_choice != 0):
+            print("Welcome to the provider menu!")
+            print("1. ")
+        
     
     #Manager menu this will work with the provider class
     def manager_menu(self):
-        pass
+        print("This is a test for the manager menu")
 
 
     def main(self):
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 
 import provider as prov
 import DatabaseApi as db
+import ChocAnInterface as ui
 def main():
     database = db.db_client()
-    provider = prov.Provider()
+    #provider = prov.Provider()
+    menu = ui.chocan_interface()
 
-    provider.read_provider_num()
+    menu.start_menu()    
+
 
 
 if __name__ == "__main__":
