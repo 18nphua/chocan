fb24ce69fa8fa6a4e75e867808bbc729b37c3840
Huan <nhuan915@gmail.com>
2024-12-04 12:49:56 -0800

    Removed unused variables from main()

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 import provider as prov
 import DatabaseApi as db
 import ChocAnInterface as ui
+
+import datetime 
+import valid 
 def main():
-    database = db.db_client()
-    #provider = prov.Provider()
     menu = ui.chocan_interface()
 
     menu.start_menu()    
-
-
 
 if __name__ == "__main__":
     main()
