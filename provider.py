04b28d95e57e9b064732a7892303d059e91bb8c6
brachang <brachang@pdx.edu>
2024-12-03 16:03:18 -0800

    Changing provider

--- provider.py
+++ provider.py
@@ -18,13 +18,13 @@ class Provider():
 
     def read_provider_num(self) -> bool: 
         validated = False
-        self.provider_num = v.read_int("Please enter your provider number: ")
+        self.provider_id = v.read_int("Please enter your provider number: ")
 
         while len(str(self.provider_id)) != PROVIDER_ID_LENGTH:
             print(f"Please enter a 9-digit number.\n")
             self.provider_id = v.read_int("Please enter your provider number: ")
 
-        self.provider_name = self.database.prov_get_name_from_id(self.self.provider_id)        
+        self.provider_name = self.database.prov_get_name_from_id(self.provider_id)        
 
         if self.provider_name is not None:
             print("Validated.")
