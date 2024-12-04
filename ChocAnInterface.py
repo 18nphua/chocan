276df3e0a4b53ca06ae63f2976dc62c9f865fb73
Huan <nhuan915@gmail.com>
2024-12-03 22:46:24 -0800

    Implementing the provider menu

--- ChocAnInterface.py
+++ ChocAnInterface.py
@@ -5,7 +5,7 @@ Desc: This is the Interface Class which will work as an interface between
       the user and the manager/provider classes.
 
 """
-import valid as v
+import valid 
 import provider as prov
 
 from ChocAnServiceCoordinator import *
@@ -18,31 +18,54 @@ class chocan_interface():
 
     #Menu that shows up on startup
     def start_menu(self):
-        term_choice = 1
+        term_choice = 0
 
         while(term_choice != 3):
-            print("Welcome to the ChocAn Terminal!\n")
+            print("ChocAn System\n")
             #print("Please choose which terminal to use\n")
-            print("1. Provider Terminal")
-            print("2. Manager Terminal")
-            print("3. Quit\n")
-            term_choice = v.read_int("Please choose which terminal to use: ")
+            print("1. Provider Terminal"
+                  "\n2. Manager Terminal"
+                  "\n3. Quit\n")
+            term_choice = valid.read_int("Please choose which terminal to use: ")
             
             if (term_choice == 1):
                 if(self.provider.read_provider_id() == True):
                     self.provider_menu()
+
             elif (term_choice == 2):
                 self.manager_menu()
+
             elif (term_choice < 1 or term_choice > 3):
                 print("\nInvalid choice selected.")
 
     #Provider menu this will work with the provider class
     def provider_menu(self):
-        prov_choice = 1
-        while (prov_choice != 0):
-            print("Welcome to the provider menu!")
-            print("1. ")
-        
+        prov_choice = 0
+        while (prov_choice != 5):
+            print("Provider Menu\n")
+            print("1. Verify Member ID"
+                  "\n2. Look Up Service Code"
+                  "\n3. Generate Bill"
+                  "\n4. Log Service"
+                  "\n5. Quit"
+                )
+
+            if (prov_choice == 1):
+                self.provider.read_member_id()
+
+            elif (prov_choice == 2):
+                self.provider.find_service_code()
+
+            elif (prov_choice == 3):
+                self.provider.generate_bill()
+  
+            elif (prov_choice == 4): 
+                self.provider.log_service()
+
+            elif (prov_choice < 1 or prov_choice > 5):
+                print("\nInvalid choice selected.")
+
+
     
     #Manager menu this will work with the provider class
     def manager_menu(self):
--- provider.py
+++ provider.py
@@ -5,27 +5,31 @@ Desc: This file contains the Provider class, which will manage all provider-spec
       and operations for the ChocAn system.
 
 """
-import valid as v
+import valid 
 import DatabaseApi as db
+import datetime
 
 PROVIDER_ID_LENGTH = 9
 MEMBER_ID_LENGTH = 9
 
 class Provider():
     def __init__(self) -> None:
+        self.service_date = " "
         self.provider_id = 0
         self.member_id = 0
+        self.service_code = 0
+        self.comments = ""
         self.database = db.db_client()
 
-    def read_provider_num(self) -> bool: 
+    def read_provider_id(self) -> bool: 
         validated = False
         provider_name = None
 
-        self.provider_id = v.read_int("Please enter your provider ID number: ")
+        self.provider_id = valid.read_int("Please enter your provider ID number: ")
 
         while len(str(self.provider_id)) != PROVIDER_ID_LENGTH:
             print(f"Please enter a 9-digit number.\n")
-            self.provider_id = v.read_int("Please enter your provider ID number: ")
+            self.provider_id = valid.read_int("Please enter your provider ID number: ")
 
         provider_name = self.database.prov_get_name_from_id(self.provider_id)        
 
@@ -39,15 +43,15 @@ class Provider():
 
         return validated
 
-    def read_member_num(self):
+    def read_member_id(self):
         validated = False
         member_status = None
 
-        self.member_id = v.read_int("Enter the member's ID number: ")
+        self.member_id = valid.read_int("Enter the member's ID number: ")
 
         while len(str(self.provider_id)) != PROVIDER_ID_LENGTH:
             print(f"Please enter a 9-digit number.\n")
-            self.provider_id = v.read_int("Enter the member's ID number: ")
+            self.provider_id = valid.read_int("Enter the member's ID number: ")
 
         #member_status = self.database.mem_get_status(self.member_id)        
 
